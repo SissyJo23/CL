bd2c4530832c8f28ce332689d73675091dcf475f
Sissy <Christy.poole@icloud.com>
2026-04-21 14:44:50 -0400

    Change routing to prioritize login page over home

--- artifacts/legal-analyzer/src/App.tsx
+++ artifacts/legal-analyzer/src/App.tsx
@@ -1,16 +1,18 @@
 import { Route, Router as WouterRouter, Switch } from "wouter";
-// We are changing 'Dashboard' to 'Home' because that is the file you actually have
-import Home from "@/pages/home"; 
+import Login from "@/pages/auth/login";
+import Home from "@/pages/home";
 
 function App() {
   return (
     <WouterRouter>
       <Switch>
-        {/* This forces the app to show the Home/Dashboard immediately */}
-        <Route path="/" component={Home} />
-        <Route path="/login" component={Home} />
-        {/* Catch-all: If anything goes wrong, just show the Home page */}
-        <Route component={Home} />
+        {/* This sets the login page as the first thing people see */}
+        <Route path="/login" component={Login} />
+        <Route path="/" component={Login} />
+        {/* This is where we will go once the login works */}
+        <Route path="/home" component={Home} />
+        {/* If a page doesn't exist, send them back to login */}
+        <Route component={Login} />
       </Switch>
     </WouterRouter>
   );
