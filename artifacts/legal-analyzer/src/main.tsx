f0316f74ee7409f4a870c80a7ce45059029b93b6
Sissy <Christy.poole@icloud.com>
2026-04-22 00:29:05 -0400

    Change auth token getter to use 'dev-token'
    Updated auth token getter to use a development token.

--- artifacts/legal-analyzer/src/main.tsx
+++ artifacts/legal-analyzer/src/main.tsx
@@ -1,10 +1,10 @@
-import { createRoot } from "react-dom/client";
+ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { setBaseUrl, setAuthTokenGetter } from "@workspace/api-client-react";
 import { getToken } from "@/lib/api";
 
 setBaseUrl("https://caselight-api.onrender.com");
-setAuthTokenGetter(() => getToken());
+setAuthTokenGetter(() => "dev-token");
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(<App />); 
