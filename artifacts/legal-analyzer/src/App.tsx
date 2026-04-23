182fb726b155d2854cbf9208429eac3a11ea37b1
Sissy <Christy.poole@icloud.com>
2026-04-23 03:14:46 -0400

    Add CaseDetail route to App component

--- artifacts/legal-analyzer/src/App.tsx
+++ artifacts/legal-analyzer/src/App.tsx
@@ -1,10 +1,10 @@
 import { Route, Router as WouterRouter, Switch, Redirect } from "wouter";
-import { useEffect } from "react";
 import { setAuthTokenGetter } from "@workspace/api-client-react";
 import Login from "@/pages/auth/login";
 import Home from "@/pages/home";
 import CasesNew from "@/pages/cases/new";
 import CasesList from "@/pages/cases/list";
+import CaseDetail from "@/pages/cases/[id]";
 import Legal from "@/pages/legal";
 
 setAuthTokenGetter(() => localStorage.getItem("authToken"));
@@ -23,6 +23,7 @@ function App() {
         <Route path="/" component={() => <ProtectedRoute component={Home} />} />
         <Route path="/home" component={() => <ProtectedRoute component={Home} />} />
         <Route path="/cases/new" component={() => <ProtectedRoute component={CasesNew} />} />
+        <Route path="/cases/:id" component={() => <ProtectedRoute component={CaseDetail} />} />
         <Route path="/cases" component={() => <ProtectedRoute component={CasesList} />} />
         <Route path="/legal" component={Legal} />
         <Route component={() => <Redirect to="/login" />} />
